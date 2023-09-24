c8b28f0dc34589c026dc9c6a35508ac1a0f38067
Ramiro Correa <98496459+ramicorrea21@users.noreply.github.com>
2023-09-24 16:28:45 +0000

    aa

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,53 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useEffect, useState } from "react";
+import "../../styles/index.css"
 
 //create your first component
 const Home = () => {
+	 const [list, setList] = useState([])
+	 const [task, setTask] = useState({})
+	 const [auxList, setAuxList] = useState([])
+	 useEffect(() => {
+	   fetch('https://playground.4geeks.com/apis/fake/todos/user/ramicorrea21')
+	   .then(response => response.json())
+	   .then(data => setList(data))
+	 }, [])
+	 
+	 const handleChange = (e) =>{
+		setTask({label : e.target.value})
+	 }
+	 const addToList = (e) =>{
+			if(e.key === "Enter"){
+				setAuxList([...auxList, task])
+				setTask({label : ""})
+				
+			}
+	 }
+
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<>
+		<div className="d-flex justify-content-center">
+			<div className="border rounder">
+				<ul>
+					<li>
+						<form onSubmit={e => e.preventDefault()}>
+							<input type="text"
+							className="form-control"
+							placeholder="What needs to be done?"
+							onChange={handleChange}
+							onKeyDown={addToList}
+							 />
+						</form>
+					</li>
+					{list.length != 0? list.map((task, i) => {
+							return(
+								<li key={i}>{task.label}</li>
+							)
+					}) : <li>Add a task</li>}
+				</ul>
+			</div>
 		</div>
+		</>
 	);
 };
 
